80ec634dfe6ea74ae75fd73440cb814bc35b41aa
n-simpkin <njsimpkin5643@gmail.com>
2025-03-08 17:37:44 +0000

    trying to figure out if I'm not actually looking for the turning point, or if I'm calculating the turning point wrong.

--- draw-hypergraph.py
+++ draw-hypergraph.py
@@ -181,19 +181,34 @@ def calculateTP(bezierInfo):
     # t = 2*(w0 - w1) / (w0 + w2 - 2 * w1)
     t = ((2 * w0) - (2 * w1)) / ((2 * w0) + (2 * w2) - (4 * w1))
     print("t", t)
+
+    if t[0] < 0:
+        t[0] = 0.0001
+    elif t[0] > 1:
+        t[0] = 1
+
+    if t[1] < 0:
+        t[1] = 0.0001
+    elif t[1] > 1:
+        t[1] = 1
+
+    t = t[1] / t[0]
+    print("div t", t)  # 0.5657
+    t = 0.5657
     # print("m", magnitude(t))
     # print("uv", makeUnitVector(t))
     # How do I get t to not be a coord pair?
     # I think the multiple soluions are bc the curve has two solutions for where the gradient is 0, however one is not in the interval so we don't care.
-    if t[0] >= 0 and t[0] <= 1:
-        t = t[0]
-    elif t[1] >= 0 and t[1] <= 1:
-        t = t[1]
-    else:
-        print("t error")  # raise error
+    # Is actually solving for both the component functions, x and y? t[0] is where the component function for x has gradient 0.
+    # if t[0] >= 0 and t[0] <= 1:
+    #     t = t[0]
+    # elif t[1] >= 0 and t[1] <= 1:
+    #     t = t[1]
+    # else:
+    #     print("t error")  # raise error
 
     # print('grad at "tp"', ((1 - t) * (w1 - w0) + (t) * (w2 - w1)))
-    print("grad at tp", (1 - t) * 2 * (w1 - w0) + t * 2 * (w2 - w1))
+    print("grad at tp", ((1 - t) * 2 * (w1 - w0)) + (t * 2 * (w2 - w1)))
     # Gives non zero so something in my gradient finding is wrong.
 
     coords = calcRationalBezierPoint(t, bezierInfo["weights"], bezierInfo["ratios"])
